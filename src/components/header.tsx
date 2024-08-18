e9ec9908d32b576835ea3846344747b5c2416682
Metaladmiral <thesecretinspiration@gmail.com>
2024-08-18 13:23:48 +0530

    Feat: added prisma & frontend for auth

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -78,6 +78,20 @@ export default function Header() {
               </div>
             </span>
           </NavLink>
+          <NavLink href="/auth/login">
+            <span className="tooltip" data-tip="Stats">
+              <div className="indicator">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 16 16"
+                  fill="currentColor"
+                  className="h-4 w-4 opacity-70"
+                >
+                  <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" />
+                </svg>
+              </div>
+            </span>
+          </NavLink>
         </ul>
       </nav>
     </>
