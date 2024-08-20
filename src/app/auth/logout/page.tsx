c4394b313017449bd6b53500a9cf00e0ae566bdb
Metaladmiral <thesecretinspiration@gmail.com>
2024-08-20 14:30:30 +0530

    Fix: logout bug fixed

--- src/app/auth/logout/page.tsx
+++ src/app/auth/logout/page.tsx
@@ -1,11 +1,14 @@
 "use client";
 
 import { redirect } from "next/navigation";
+import Router from "next/router";
 import { useEffect } from "react";
 export default function Page() {
   useEffect(() => {
     if (localStorage.getItem("jwt") && localStorage.getItem("jwt") !== "") {
       localStorage.removeItem("jwt");
+      localStorage.removeItem("cart-storage");
+      window.location.reload();
     }
     redirect("/auth/login");
   }, []);
